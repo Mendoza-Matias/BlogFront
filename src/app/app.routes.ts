5ba074515471339cdca8153398b5c2c19464f97a
Matias Mendoza <mendoza.e.matias@gmail.com>
2025-01-15 22:12:08 -0300

    created the first components and integrate bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PostsComponent } from './components/posts/posts.component';
+import { TagsComponent } from './components/tags/tags.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'posts', component: PostsComponent
+    },
+    {
+        path: 'tags', component: TagsComponent
+    }
+];
+
